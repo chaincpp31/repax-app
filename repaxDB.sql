04d700e45790c4dc183c6ce07618a704488391b1
chaincpp31 <chaincpp31@gmail.com>
2020-08-26 16:18:14 +0700

    edit repaxDB

--- repaxDB.sql
+++ repaxDB.sql
@@ -2,6 +2,44 @@ CREATE DATABASE IF NOT EXISTS `repaxDB`;
 
 use `repaxDB`;
 
+DROP TABLE IF EXISTS `Admin-manage`;
+DROP TABLE IF EXISTS `Type-job-Clients technician`;
+DROP TABLE IF EXISTS `Clients-Admin`;
+DROP TABLE IF EXISTS `Clients`;
+DROP TABLE IF EXISTS `Type-job`;
+DROP TABLE IF EXISTS `Clients-technician`;
+
+CREATE TABLE `Admin-manage`(
+    `admin-id` BIGINT AUTO_INCREMENT,
+    `user-tecnician-id` INT(5),
+    `type-id` INT(5),
+    `user-id` INT(5)
+
+    PRIMARY KEY (`admin-id`)
+    FOREIGN KEY (`admin-id`),(`user-id`),(`type-id`),(`user-tecnician-id`)
+        CONSTRAINT `Clients-Admin_ibfk_1` FOREIGN KEY (`admin-id`) REFERENCES `Clients-Admin`,`admin-id`,
+            CONSTRAINT `Type-job_ibfk_2` FOREIGN KEY (`type-id`) REFERENCES `Type-job`,`type-id`,
+                CONSTRAINT `Clients_ibfk_3` FOREIGN KEY (`user-id`) REFERENCES `Clients`,`user-id`,
+                    CONSTRAINT `Clients-tecnincian_ibfk_4` FOREIGN KEY (`user-tecnician-id`) REFERENCES `Clients-technician`,`user-technician-id`
+
+
+
+
+)ENGINE=InnoDB DEFAULT CHASET utf8mb4;
+
+CREATE TABLE `Clients-Admin`(
+    `admin-id` INT(5),
+    `user-name` VARCHAR(50),
+    `password` VARCHAR(50),
+    `first-name` VARCHAR(50),
+    `last-name` VARCHAR(50),
+    `gender` VARCHAR(10),
+    `age` INT(10),
+    `address` VARCHAR(50)
+
+    PRIMARY KEY (`admin-id`)
+)ENGINE=InnoDB DEFAULT CHASET utf8mb4;
+
 CREATE TABLE `Clients`(
     `user-id` BIGINT AUTO_INCREMENT,
     `user-name` VARCHAR(50) NOT NULL ,
@@ -14,7 +52,7 @@ CREATE TABLE `Clients`(
     PRIMARY KEY (`user-id`)
 )ENGINE=InnoDB DEFAULT CHASET utf8mb4;
 
-CREATE TABLE `Type job`(
+CREATE TABLE `Type-job`(
     `type-id` BIGINT(5),
     `user-id` INT(5) NOT NULL,
     `electric` VARCHAR(50) NOT NULL,
@@ -40,7 +78,14 @@ CREATE TABLE `Clients-technician`(
     `address` VARCHAR(50) NOT NULL,
     `type-job` VARCHAR(50)
 
-    PRIMARY KEY (`user-technician-id`),
-    CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `Teachers`,`id`,
-     CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `Groups`,`id`
+    PRIMARY KEY (`user-technician-id`)
+)
+
+CREATE TABLE `Type job-Clients-technician`(
+    `type-id` INT(5),
+    `user-tecnician` INT(5)
+
+    PRIMARY KEY (`type-id`),
+    FOREIGN KEY (`type-id`),(`user-tecnician`)
+    CONSTRAINT `Type-job_ibfk_5` FOREIGN KEY (`type-id`) REFERENCES `Type-job`,`type-id`
 )
